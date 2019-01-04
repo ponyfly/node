332cf93bdcca6e32b46e113516067f70a1b51bbc
zechaozhang <zechao.zhang@langtaojin.com>
2019-01-04 19:07:03 +0800

    aa

--- koastudy/puppeteer/html2img/index.js
+++ koastudy/puppeteer/html2img/index.js
@@ -1,23 +1,27 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
+const path = require('path');
 
-async function createIndex() {
-	const browser = await puppeteer.launch({ headless: false })
+(async function createIndex() {
+	console.time('grab')
+	const browser = await puppeteer.launch()
 	const page = await browser.newPage()
-	page.setViewport({
-		width: 1024,
-		height: 1600,
-	})
-	await page.goto('https://www.google.com')
+	// page.setViewport({
+	// 	width: 1024,
+	// 	height: 1600,
+	// })
+	await page.goto('https://www.baidu.com')
 	await page.screenshot({
-		path: './imgs/google.png',
-		clip: {
-			x: 0,
-			y: 0,
-			width: 750,
-			height: 600,
-		},
+		path: path.resolve(__dirname, './imgs/google.jpg'),
+		// clip: {
+		// 	x: 0,
+		// 	y: 0,
+		// 	width: 750,
+		// 	height: 600,
+		// },
 	})
+	// await browser.close()
+	console.timeEnd('grab')
+}())
 
-	await browser.close()
-}
-createIndex()
+// 1629 1610 1654 1940 1666 1829 1805 1657 1901 1844
+// average 1748 , faster Than phantomjs:700ms
